9f333596056311e0d1d0c29ae78c173677f2ab43
Andrew Wilkey <awilkey@iastate.edu>
2015-06-04 15:56:45 -0500

    updated fullscreen to reflect current API

--- js/main.js
+++ js/main.js
@@ -2,49 +2,59 @@ define([
            'dojo/_base/declare',
            'dojo/query',
 
-           'dijit/MenuItem',
-	   'dijit/form/Button',
+	   	   'dijit/form/Button',
            'JBrowse/Plugin'
        ],
        function(
            declare,
            query,
-           dMenuItem,
-	   dijitButton,
+	   	   
+		   dijitButton,
            JBrowsePlugin
        ) {
 return declare( JBrowsePlugin,
 {
     constructor: function( args ) {
-      	var thisDoc = this.document; 
-	var thisBrowser = this.browser;
+		
+		var thisBrowser = this.browser;
         var makeFull = this.makeFullscreenButton();
-	thisBrowser.afterMilestone('initView', function() {
 	
-		var myMenu = query(".menuBar");
-		// do anything you need to initialize your plugin here
+		thisBrowser.afterMilestone('initView', function() {
+	
+			var myMenu = thisBrowser.menuBar;
+			
         	console.log( "fullscreen plugin starting" );
 
-		console.log("genometest");
+			myMenu.appendChild(makeFull);
 		
-		thisBrowser.menuBar.appendChild(makeFull);
 		})
+        
+		console.log( "fullscreen plugin added" );
 	},
 
     makeFullscreenButton: function () {
 	
 	var goFull = function(){
-		var ele = document.body; 
-		if (ele.requestFullscreen){
+		var ele = document.body;
+		// Recent api changes mean that browser might be looking for Fullscreen
+		// or FullScreen
+
+		if (ele.requestFullScreen){
 			ele.requestFullScreen();
+		} else if (ele.requestFullscreen){
+			ele.requestFullscreen();
 		} else if (ele.msRequestFullScreen){
-			ele.msRequestFullScreen();	
+			ele.msRequestFullScreen();
+		} else if (ele.msRequestFullscreen){
+			ele.msRequestFullscreen();
 		} else if (ele.mozRequestFullScreen){
 			ele.mozRequestFullScreen();
 		} else if (ele.webkitRequestFullScreen){
 			ele.webkitRequestFullScreen();
+		} else if (ele.webkitRequestFullscreen){
+			ele.webkitRequestfullscreen();
 		}
-    	};
+	};
 
 	var selectFullButton = new dijitButton({
 		className :"fullscreen-button",
