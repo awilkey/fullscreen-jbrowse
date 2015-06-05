6b63357c0db0dca01fea266872950de7dad1d016
Andrew Wilkey <awilkey@iastate.edu>
2015-06-05 12:07:51 -0500

    Button toggles fullscreen mode and only appears if browser or iframe supports it.

--- js/main.js
+++ js/main.js
@@ -18,7 +18,19 @@ return declare( JBrowsePlugin,
 		
 		var thisBrowser = this.browser;
         var makeFull = this.makeFullscreenButton();
-	
+		
+		if (!document.mozFullScreenEnabled 
+			&& !document.webkitFullScreenEnabled 
+			&& !document.webkitFullscreenEnabled
+			&& !document.msFullScreenEnabled
+			&& !document.msFullscreenEnabled
+			&& !document.fullScreenEnabled
+			&& !document.fullscreenEnabled
+		   ){
+				console.log("fullscreen is not supported by your browser.");
+				return;
+			}
+
 		thisBrowser.afterMilestone('initView', function() {
 	
 			var myMenu = thisBrowser.menuBar;
@@ -36,24 +48,58 @@ return declare( JBrowsePlugin,
 	
 	var goFull = function(){
 		var ele = document.body;
+		var doc = document;
 		// Recent api changes mean that browser might be looking for Fullscreen
-		// or FullScreen
+		// or FullScreen 
 
-		if (ele.requestFullScreen){
-			ele.requestFullScreen();
-		} else if (ele.requestFullscreen){
-			ele.requestFullscreen();
-		} else if (ele.msRequestFullScreen){
-			ele.msRequestFullScreen();
-		} else if (ele.msRequestFullscreen){
-			ele.msRequestFullscreen();
-		} else if (ele.mozRequestFullScreen){
-			ele.mozRequestFullScreen();
-		} else if (ele.webkitRequestFullScreen){
-			ele.webkitRequestFullScreen();
-		} else if (ele.webkitRequestFullscreen){
-			ele.webkitRequestfullscreen();
-		}
+
+	
+		if(		!doc.fullscreen
+			&& 	!doc.fullScreen	
+			&& 	!doc.msFullscreenElement 
+			&& 	!doc.msFullScreenElement
+			&& 	!doc.mozFullScreen
+			&& 	!doc.webkitFullscreenElement
+			&& 	!doc.webkitFullScreenElement){
+
+			console.log("entering fullscreen");
+			
+			if (ele.requestFullScreen){
+				ele.requestFullScreen();
+			} else if (ele.requestFullscreen){
+				ele.requestFullscreen();
+			} else if (ele.msRequestFullScreen){
+				ele.msRequestFullScreen();
+			} else if (ele.msRequestFullscreen){
+				ele.msRequestFullscreen();
+			} else if (ele.mozRequestFullScreen){
+				ele.mozRequestFullScreen();
+			} else if (ele.webkitRequestFullScreen){
+				ele.webkitRequestFullScreen();
+			} else if (ele.webkitRequestFullscreen){
+				ele.webkitRequestfullscreen();
+			}
+		// If already fullscreen toggle
+		 } else {
+		 	
+		 	console.log("closing fullscreen");
+		 	
+		 	if (doc.exitFullScreen){
+		 		doc.exitFullScreen();
+		 	} else if (doc.exitFullscreen){
+		 		doc.exitFullscreen();
+		 	} else if (doc.msExitFullScreen){
+		 		doc.msExitFullScreen();
+		 	} else if (doc.msExitFullscreen){
+		 		doc.msExitFullscreen();
+		 	} else if (doc.mozCancelFullScreen){
+		 		doc.mozCancelFullScreen();
+		 	} else if (doc.webkitExitFullScreen){
+		 		doc.webkitExitFullScreen();
+		 	} else if (doc.webkitExitFullscreen){
+		 		doc.webkitExitFullscreen();
+			}
+		 }
 	};
 
 	var selectFullButton = new dijitButton({
@@ -61,6 +107,7 @@ return declare( JBrowsePlugin,
 		innerHTML:"Fullscreen",
 		title: "Enable Fullscreen",
 		onClick : function(){
+	
 			goFull();
 		
 		}
